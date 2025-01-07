d9635e45f5f47b0532f4faa3cdf45356235e443f
melis <melisbyrm@gmail.com>
2025-01-07 22:50:21 +0100

    sh files updated

--- run_critical_cases.sh
+++ run_critical_cases.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-behave --tags="@priority-High" --tags="~@skip" \
-       --format behave_html_formatter:HTMLFormatter \
-       --outfile reports/critical_report.html
+behave --tags="@priority-High" --tags="~@skip" --format behave_html_formatter:HTMLFormatter --outfile reports/critical_report.html
 
 
--- run_medium_priority_tests.sh
+++ run_medium_priority_tests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-behave --tags="@priority-Medium" --tags="~@skip" \
-       --format behave_html_formatter:HTMLFormatter \
-       --outfile reports/medium_report.html
+behave --tags="@priority-Medium" --tags="~@skip" --format behave_html_formatter:HTMLFormatter --outfile reports/medium_report.html
 
