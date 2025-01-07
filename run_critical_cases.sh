da0e07ee253f431e4548691a262cf7e58d813386
melis <melisbyrm@gmail.com>
2025-01-07 20:37:09 +0100

    Allure reports generation and adding to github pages added

--- run_critical_cases.sh
+++ run_critical_cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-behave --tags="@priority-High" --tags="~@skip"
+behave --tags="@priority-High" --tags="~@skip" --format allure_behave.formatter:AllureFormatter --outfile=allure-results
 
 
