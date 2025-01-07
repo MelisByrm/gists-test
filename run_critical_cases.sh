df10edbae5213ed2254b1f09b79d8bcbd47a4374
melis <melisbyrm@gmail.com>
2025-01-07 22:26:42 +0100

    report implemented

--- run_critical_cases.sh
+++ run_critical_cases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-behave --tags="@priority-High" --tags="~@skip"
+behave --tags="@priority-High" --tags="~@skip" --format html --outfile critical_report.html
 
 
--- run_medium_priority_tests.sh
+++ run_medium_priority_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-behave --tags="@priority-Medium" --tags="~@skip"
+behave --tags="@priority-Medium" --tags="~@skip" --format html --outfile medium_report.html
 
